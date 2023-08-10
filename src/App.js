fbb1ee40d4979a45e0f74007657268be3555a645
Elisa Alvarez <elisaalvarez211@gmail.com>
2023-08-10 01:46:58 -0700

    Random Color Generator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Esignature from './projects/Esignature';
+import RandomizeColors from './projects/RandomizeColors';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-    <Esignature/>
+    {/* <Esignature/> */}
+    <RandomizeColors/>
     </div>
   );
 }
