bfc90b26b6113910d1cc984892aeb142804742f5
Elisa Alvarez <elisaalvarez211@gmail.com>
2023-08-16 01:19:57 -0700

    added like count

--- src/projects/components/Card.jsx
+++ src/projects/components/Card.jsx
@@ -7,21 +7,28 @@ import {BsBalloonHeartFill, BsBalloonHeart} from 'react-icons/bs'
 import {AiOutlineComment} from 'react-icons/ai'
 export default function Card() {
   const [like, setLikes] =useState(false)
+  const [count, setCount] = useState(0)
   
-  const handleLikes = (e) =>{
-    !like ?setLikes(true): setLikes(false)
+  const handleLikes = () =>{
+    if(!like){
+      setCount(count+1)
+      setLikes(true)
+    }else{
+      setCount(count -1)
+      setLikes(false)
+    }
   }
   return (
-    <div className="card margin-auto" style={{width: "40vw", cursor:"pointer"}}>
+    <div className="card margin-auto" style={{width: "25%", cursor:"pointer"}}>
         <div className='card-header d-flex' style={{justifyItems:"center", justifyContent:"space-between"}}>
                 <GiDogBowl/>
                 <small>DoggyDog</small>
                 <LuMenu/>
         </div>
-        <img src={DogPic} alt='profile photo of doggy dog' style={{ width: '98%', height:'60vh'}} />
-        <div className="card-footer d-flex" style={{justifyContent:"space-between", height:"15%"}}>
+        <img src={DogPic} alt='profile photo of doggy dog' style={{ width: '98%', height:'fit-content'}} />
+        <div className="card-footer d-flex" style={{justifyContent:"space-between", height:"1.5rem", padding: '0.9rem'}}>
             <AiOutlineComment/>
-            <Title text={"Likes"} classes={"subtitle"}/>
+            <Title text={"Likes "+ count} classes={"subtitle"}/>
             { like ?<BsBalloonHeartFill className='text-danger' onClick={handleLikes}/>
             :<BsBalloonHeart onClick={handleLikes}/>}
         </div>
